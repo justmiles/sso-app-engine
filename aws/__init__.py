104c698ef9bd2cd931697ec41f3db02b8802c113
Miles Maddox <miles@findingapogee.com>
2014-05-06 21:44:49 +0000

    5/6

--- aws/__init__.py
+++ aws/__init__.py
@@ -1,3 +1,5 @@
+# http://docs.aws.amazon.com/STS/latest/UsingSTS/STSMgmtConsole-manualURL.html
+
 import cgi
 from google.appengine.api import users
 import webapp2
@@ -5,4 +7,5 @@ import webapp2
 class AWSAuth(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
-        self.redirect(""https://findingapogee.signin.aws.amazon.com/console")
+        # TODO: parse settings.cfg for redirect url (alias)
+        self.redirect("https://findingapogee.signin.aws.amazon.com/console")
