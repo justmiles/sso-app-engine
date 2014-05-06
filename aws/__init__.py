a9238c5009be664f00d9df71a4b90aab945357ca
Miles Maddox <miles@findingapogee.com>
2014-05-06 20:17:27 +0000

    cleanup

--- aws/__init__.py
+++ aws/__init__.py
@@ -5,4 +5,4 @@ import webapp2
 class AWSAuth(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
-        self.redirect("https://edointeractive.signin.aws.amazon.com/console")
+        self.redirect(""https://findingapogee.signin.aws.amazon.com/console")
